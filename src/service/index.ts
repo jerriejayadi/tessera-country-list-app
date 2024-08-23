caaef36b56ce7b2e991c733c7fe68d2a9a0f1b13
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-08-23 12:10:07 +0800

    [FIX] Proxy to bypass cors error

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,7 +2,7 @@ import { CountryProps } from "@/types";
 
 export async function getCountryList() {
   const response = await fetch(
-    "https://yourtessera-backend-api-staging.up.railway.app/locations/countries",
+    "/apis",
     {
       headers: {
         Accept: "*/*",
